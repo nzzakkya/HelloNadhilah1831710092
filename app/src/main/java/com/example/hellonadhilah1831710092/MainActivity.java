0a70b65112eb5dfc7d596d5f832948e611b9bed8
nzzakkya <n.zakkya2@gmail.com>
2020-02-05 09:41:13 +0700

    mengganti dengan nama lengkap

--- app/src/main/java/com/example/hellonadhilah1831710092/MainActivity.java
+++ app/src/main/java/com/example/hellonadhilah1831710092/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);1
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 }
